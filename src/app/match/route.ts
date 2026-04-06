6ccc60a12b1aa1271c15ac59cf34bbd50187ec5e
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2026-04-06 14:55:35 -0400

    add get match endpoint

---
+++ src/app/match/route.ts
@@ -0,0 +1,50 @@
+import { WebcastType } from "@/generated/prisma/client";
+import { DEFAULT, get, Preferences } from "@/lib/preference";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: Request) {
+    const preferences = ((await request.json()) ?? DEFAULT) as Preferences;
+    const time = new Date();
+    time.setDate(3);
+    const matches = await prisma.match.findMany({
+        where: {
+            // done: false,
+            time: {
+                gte: time,
+                lte: new Date(time.getTime() + preferences.maxTime),
+            },
+        },
+        include: {
+            event: {
+                include: {
+                    webcasts: true,
+                },
+            },
+        },
+    });
+    if (matches.length === 0)
+        return Response.json({ status: "noMatch", match: null });
+    const selectedMatch = get(matches, preferences, time);
+    const webcasts: { type: WebcastType; channel: string }[] = [];
+    for (const webcast of selectedMatch.event.webcasts) {
+        if (
+            webcast.date === null ||
+            (webcast.date.getUTCDate() === time.getUTCDate() &&
+                webcast.date.getUTCMonth() === time.getUTCMonth() &&
+                webcast.date.getUTCFullYear() === time.getUTCFullYear())
+        )
+            webcasts.push({ type: webcast.type, channel: webcast.channel });
+    }
+    const {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        event: { webcasts: _, id: __, updatedAt: ___, ...event },
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        id: ____,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        eventId: _____,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        asOfTime: ______,
+        ...match
+    } = selectedMatch;
+    return Response.json({ status: "success", match, webcasts, event });
+}
