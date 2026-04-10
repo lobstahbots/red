c855562ab130cc9518fd1044c5342902df787e62
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2026-04-10 14:26:10 -0400

    make gooder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
 import Stream from "@/components/Stream";
 import { useState } from "react";
@@ -9,29 +8,46 @@ export default function Home() {
         null,
     );
     const [channel, setChannel] = useState<string>("");
+    const [match, setMatch] = useState<{
+        event: { name: string };
+        key: string;
+    } | null>(null);
+    const loadMatch = async () => {
+        const match = await fetch("/match", {
+            method: "POST",
+            body: "null",
+        });
+        const data = await match.json();
+        if (data.status === "success") {
+            setStreamType(data.webcasts[0].type);
+            setChannel(data.webcasts[0].channel);
+            setMatch(data);
+        }
+    };
     return (
         <div className={styles.page}>
             {streamType === null ? (
                 <div className={styles.buttons}>
-                    <button
-                        className={styles.button}
-                        onClick={async () => {
-                            const match = await fetch("/match", {
-                                method: "POST",
-                                body: "null",
-                            });
-                            const data = await match.json();
-                            if (data.status === "success") {
-                                setStreamType(data.webcasts[0].type);
-                                setChannel(data.webcasts[0].channel);
-                            }
-                        }}
-                    >
+                    <button className={styles.button} onClick={loadMatch}>
                         Load match
                     </button>
                 </div>
             ) : null}
-            {streamType && <Stream type={streamType} channel={channel} />}
+            {streamType && match && (
+                <>
+                    <Stream type={streamType} channel={channel} />
+                    <div className={styles.matchInfo}>
+                        <h3>{match.event.name}</h3>
+                        <p>{match.key}</p>
+                        <button
+                            className={`${styles.button} ${styles.secondaryButton}`}
+                            onClick={loadMatch}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
