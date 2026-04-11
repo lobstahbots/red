1f09b865f2091432c24f5981f4ec6c2348b3f040
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2026-04-11 11:14:35 -0400

    subscribe on server

--- src/lib/match.ts
+++ src/lib/match.ts
@@ -15,6 +15,7 @@ export async function markDone(matchKey: string) {
     client.connect();
     await client
         .channel(matchKey.split("_")[0])
+        .subscribe()
         .send({
             type: "broadcast",
             event: "match_done",
